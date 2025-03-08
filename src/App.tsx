5cfca9acaeeb73338aae7b4110b77f4bbc702766
LauraBLei <laura.blei@outlook.com>
2025-03-08 23:35:33 +0100

    set up and made home page

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./index.css";
+import "./fonts.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Layout } from "./components/layout";
+import { RunHomePage } from "./pages/homePage";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<RunHomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/homePage.tsx
@@ -0,0 +1,41 @@
+export const RunHomePage = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="max-w-[1400px]">
+        <div className="h-[200px] sm:h-[300px] md:h-[400px]">
+          <img
+            className="object-cover w-full h-full"
+            src="./frontpageImage.png"
+            alt=""
+          />
+        </div>
+        <div className="w-full items-center  flex max-w-[780px] relative">
+          <img
+            className="max-w-[50vw] w-full absolute z-50"
+            src="./flowerImage.png"
+            alt=""
+          />
+        </div>
+        <div className="mt-[120px] sm:mt-0 sm:justify-self-end  mr-5 p-5 text-center text-customGreen flex flex-col gap-5">
+          <div>
+            <p className="font-primary text-5xl lg:text-7xl">
+              Save{" "}
+              <span className="font-secondary text-6xl lg:text-8xl">The</span>{" "}
+              Date
+            </p>
+            <p className="font-primary text-2xl lg:text-3xl">
+              Hold av datoen for kommende bryllup
+            </p>
+          </div>
+          <p className="font-primary text-5xl lg:text-7xl">
+            Lasse <span className="font-secondary text-6xl lg:text-8xl">&</span>{" "}
+            Laura
+          </p>
+          <p className="font-primary text-3xl lg:text-5xl">
+            Lørdag d. 30 mai 2026
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
