3095c195622d3d3ebe7407d5b3f5626a5dde152c
LauraBLei <laura.blei@outlook.com>
2025-03-09 00:36:57 +0100

    hidden menu for now

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { MenuIcon } from "lucide-react";
 export const Header = () => {
   return (
     <div className="justify-self-end absolute right-0">
-      <MenuIcon className="w-[30px] md:w-[50px] h-auto my-2 mx-2 text-customGreen cursor-not-allowed" />
+      <MenuIcon className="hidden w-[30px] md:w-[50px] h-auto my-2 mx-2 text-customGreen cursor-not-allowed" />
     </div>
   );
 };
