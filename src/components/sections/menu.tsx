2fe16da3b57e7308874e4b7ddb05cfde498c0507
LauraBLei <laura.blei@outlook.com>
2025-07-04 15:42:15 +0200

    menu section

--- src/components/sections/menu.tsx
+++ src/components/sections/menu.tsx
@@ -1,7 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 export const MenuSection = () => {
+  const { t } = useTranslation();
   return (
     <div>
-      <h1>Menu</h1>
+      <h1 className="headline">{t("menu.headline")}</h1>
+      <p className="text-center text-2xl py-20">{t("menu.text")}</p>
+      <div className="w-full max-w-[700px] place-self-center border-b-1 border-white"></div>
     </div>
   );
 };
