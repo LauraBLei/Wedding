8380a87678faeca414db4ab982ead662eeab0ace
LauraBLei <laura.blei@outlook.com>
2025-07-04 18:10:58 +0200

    added flower decoration to accomodation

--- src/components/sections/overnatning.tsx
+++ src/components/sections/overnatning.tsx
@@ -19,46 +19,57 @@ export const OvernatningSection = () => {
 
   return (
     <div>
-      <h1 className="headline mb-5">{t("rooms.headline")}</h1>
-      <div className="flex flex-col gap-2">
-        <p>{t("rooms.textOne")}</p>
-        <p>{t("rooms.textTwo")}</p>
-        <p>{t("rooms.textThree")}</p>
-        <p>{t("rooms.textFour")}</p>
-      </div>
-      <div className="overflow-x-auto mt-6 mb-4 text-xs md:text-lg lg:text-2xl">
-        {table.valid ? (
-          <table className="min-w-[350px] w-full rounded-lg">
-            <thead>
-              <tr>
-                {table.headers.map((header, idx) => (
-                  <th key={idx} className="px-4 py-2  text-left">
-                    {header}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {table.rows.map((row, i) => (
-                <tr key={i}>
-                  {row.map((cell, j) => (
-                    <td key={j} className="px-4 py-2 ">
-                      {cell}
-                    </td>
+      <div className="md:mr-50 lg:mr-70">
+        <h1 className="headline mb-5">{t("rooms.headline")}</h1>
+        <div className="flex flex-col gap-2">
+          <p>{t("rooms.textOne")}</p>
+          <p>{t("rooms.textTwo")}</p>
+          <p>{t("rooms.textThree")}</p>
+          <p>{t("rooms.textFour")}</p>
+        </div>
+        <div className="overflow-x-auto mt-6 mb-4 text-xs md:text-lg">
+          {table.valid ? (
+            <table className="min-w-[350px] w-full rounded-lg">
+              <thead>
+                <tr>
+                  {table.headers.map((header, idx) => (
+                    <th key={idx} className="px-4 py-2  text-left">
+                      {header}
+                    </th>
                   ))}
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <div className="text-red-500">
-            Table data missing or invalid in translation file.
-          </div>
-        )}
+              </thead>
+              <tbody>
+                {table.rows.map((row, i) => (
+                  <tr key={i}>
+                    {row.map((cell, j) => (
+                      <td key={j} className="px-4 py-2 ">
+                        {cell}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div className="text-red-500">
+              Table data missing or invalid in translation file.
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-sm text-gray-200/80">{t("rooms.small_text")}</p>
+          <p className="text-sm text-gray-200/80">
+            {t("rooms.small_text_two")}
+          </p>
+        </div>
       </div>
-      <div className="flex flex-col gap-2">
-        <p className="text-sm text-gray-200/80">{t("rooms.small_text")}</p>
-        <p className="text-sm text-gray-200/80">{t("rooms.small_text_two")}</p>
+      <div>
+        <img
+          className="hidden md:block absolute right-0 md:h-[300px] lg:h-[400px] rotate-y-180 place-self-end"
+          src="/flowers.png"
+          alt=""
+        />
       </div>
     </div>
   );
