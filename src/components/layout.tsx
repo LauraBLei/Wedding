63a8a435ae125cf42f7c1239b3a74a6aab7a83ea
LauraBLei <laura.blei@outlook.com>
2026-07-17 09:05:35 +0200

    removed unused files

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router";
-import { Sidebar } from "./sidebar";
 
 export const Layout = () => {
   return (
     <div className="w-full flex">
-      <Sidebar />
       <div className="w-full mt-20">
         <Outlet />
       </div>
